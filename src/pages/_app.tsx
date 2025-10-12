39639e3e8053c062c75e5cc0f4d051296aca66b0
softgenai <admin@softgen.ai>
2025-10-12 02:17:36 +0000

    feat(layout): Integrate global Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
-import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@/contexts/ThemeProvider";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { Header } from "@/components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Component {...pageProps} />
-      <Toaster />
-    </>
+    <ThemeProvider>
+      <AuthProvider>
+        <Header />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
