2e826f524a12cf0ed61adec6ae3d1320d91f7edf
Bobby Nicholson <bobby@skylight-digital.co.za>
2026-04-21 20:52:41 +0000

    feat(db): establish backend schema as source of truth

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -11,5 +11,5 @@ export type Tables<T extends keyof Database['public']['Tables']> = Database['pub
 export type Enums<T extends keyof Database['public']['Enums']> = Database['public']['Enums'][T];
 export type Functions<T extends keyof Database['public']['Functions']> = Database['public']['Functions'][T];
 
-// Project reference: vsuyzovzqtrngorpqnhy
-// Last updated: 2026-04-21T16:18:51.037863
+// Project reference: ypwxsmytkvaefmmlkspf
+// Last updated: 2025-10-21T09:37:21.043062
